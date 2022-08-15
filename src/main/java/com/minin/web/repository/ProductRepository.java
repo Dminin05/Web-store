52b91f408a83f5f89593abb819805c6f5eb81305
Dima Minin <dminin0055@gmail.com>
2022-08-15 12:45:57 +0300

    first commit

---
+++ src/main/java/com/minin/web/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.minin.web.repository;
+
+import com.minin.web.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/minin/web/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.minin.web.service;
+
+import com.minin.web.model.Category;
+import com.minin.web.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Optional<Category> findByTitle(String title) {
+        return categoryRepository.findByTitle(title);
+    }
+
+}
