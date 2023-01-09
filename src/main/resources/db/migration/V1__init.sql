42ca0ca3ce7567792e693ee0cf223582aaaca69d
Dima Minin <dminin0055@gmail.com>
2023-01-09 13:26:18 +0300

    security

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -28,8 +28,8 @@ values ('ROLE_USER'),
        ('ROLE_ADMIN');
 
 insert into users (username, password, email)
-values ('user', '$2y$10$D7wpehwGf4avzyy6.pwZiuzcHyTkM9U1br2ZH.SXVwZavuJhvhtyO', 'usertest@gmail.com'),
-       ('admin', '$2y$10$6fXxe//uQw5raumo5tnqpOEDcLloQx62kjP/XLHRMSNGsseVsQEby', 'admintest@gmail.com');
+values ('user', '$2y$10$D7wpehwGf4avzyy6.pwZiuzcHyTkM9U1br2ZH.SXVwZavuJhvhtyO', 'usertest@gmail.com'), /*password = user*/
+       ('admin', '$2y$10$6fXxe//uQw5raumo5tnqpOEDcLloQx62kjP/XLHRMSNGsseVsQEby', 'admintest@gmail.com');/*password = admin*/
 
 insert into users_roles (user_id, role_id)
 values (1,1), (2, 2);
